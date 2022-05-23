c06553de1656b03329708cdb3b984598f798847f
Andrei Naumenko <6110814n@gmail.com>
2022-05-23 14:30:33 +0300

    save

--- js/index.js
+++ js/index.js
@@ -1,14 +1,3 @@
-// // Web app's Firebase configuration.
-// const firebaseConfig = {
-//    apiKey: "AIzaSyBLLIecNGd-FqgkE2SZ9L3HcvZ-ZdjzA3Q",
-//    authDomain: "planetarium-2d7ef.firebaseapp.com",
-//    projectId: "planetarium-2d7ef",
-//    storageBucket: "planetarium-2d7ef.appspot.com",
-//    messagingSenderId: "97830966086",
-//    appId: "1:97830966086:web:f0e92e819a1d3f803a0b47",
-//    measurementId: "G-856QHNZPGK"
-// };
-
 
 const components = {
    header: Header,
@@ -801,7 +790,7 @@ if (user) {
 // Logout user from database.
 document.querySelector("#logout-button").addEventListener("click", () => {
 auth.signOut();
-//alert("Signed out");
+   alert("Signed out");
 });
 
 // Setting EventListener for "enter" key.
